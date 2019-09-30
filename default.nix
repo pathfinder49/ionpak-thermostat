7f32591441af170984aea3ccac70436e430bf123
Astro <astro@spaceboyz.net>
2019-09-30 20:43:03 +0200

    default.nix: pin rust-nightly, build firmware

--- default.nix
+++ default.nix
@@ -1,5 +1,6 @@
 { # Use master branch of the overlay by default
   mozillaOverlay ? import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz),
+  rustManifest ? ./channel-rust-nightly.toml,
 }:
 
 let
@@ -7,21 +8,53 @@ let
 in
 with pkgs;
 let
-  rustcSrc = fetchgit {
+  rustcSrc = pkgs.fetchgit {
     url = https://github.com/rust-lang/rust.git;
-    # master of 2019-08-31
-    rev = "b3146549abf25818fecfc7555f35358a948e27ad";
-    sha256 = "1db3g1iq6ba5pdasffay1bpywdibv83z5nwp2dzi0fxvz5bqx1gi";
+    # master of 2019-09-25
+    rev = "37538aa1365d1f8a10770a7d15c95b3167c8db57";
+    sha256 = "1nvddkxwvrsvyx187s5mwj4fwsf26xd4vr6ba1kfy7m2fj7w79hq";
     fetchSubmodules = true;
   };
-  targets = [
-  ];
+  target = "thumbv7em-none-eabihf";
+  targets = [ target ];
+  rustChannelOfTargets = _channel: _date: targets:
+    (pkgs.lib.rustLib.fromManifestFile rustManifest {
+      inherit (pkgs) stdenv fetchurl patchelf;
+    }).rust.override { inherit targets; };
   rust =
     rustChannelOfTargets "nightly" null targets;
   rustPlatform = recurseIntoAttrs (makeRustPlatform {
     rustc = rust // { src = rustcSrc; };
     cargo = rust;
   });
+  gcc = pkgsCross.armv7l-hf-multiplatform.buildPackages.gcc;
+  xbuildRustPackage = attrs:
+    let
+      buildPkg = rustPlatform.buildRustPackage attrs;
+    in
+    buildPkg.overrideAttrs ({ name, nativeBuildInputs, ... }: {
+      nativeBuildInputs =
+        nativeBuildInputs ++ [ cargo-xbuild ];
+      buildPhase = ''
+        cargo xbuild --release --frozen
+      '';
+      XARGO_RUST_SRC = "${rustcSrc}/src";
+      installPhase = ''
+        mkdir $out
+        cp target/${target}/release/${name} $out/${name}.elf
+      '';
+    });
+  firmware = xbuildRustPackage {
+    name = "firmware";
+    src = ./firmware;
+    cargoSha256 = "13nk3m9s7fy4anl89x5q88b7iar9y48ricj3k5ap741g2cll02dv";
+    nativeBuildInputs = [
+      gcc
+    ];
+    "CC_${target}" = "${gcc}/bin/armv7l-unknown-linux-gnueabihf-gcc";
+    RUST_COMPILER_RT_ROOT = "${rustcSrc}/src/llvm-project/compiler-rt";
+    doCheck = false;
+  };
 in {
-  inherit pkgs rustPlatform rustcSrc;
+  inherit pkgs rustPlatform rustcSrc gcc firmware;
 }
--- firmware/build.rs
+++ firmware/build.rs
@@ -8,45 +8,6 @@ use std::process::Command;
 
 use walkdir::WalkDir;
 
-fn git_describe() {
-    let git_dir = Path::new("../.git");
-
-    println!("cargo:rerun-if-changed={}", git_dir.join("HEAD").display());
-    for entry in WalkDir::new(git_dir.join("refs")) {
-        let entry = entry.unwrap();
-        println!("cargo:rerun-if-changed={}", entry.path().display());
-    }
-
-    let version;
-    if git_dir.exists() {
-        let git_describe =
-            Command::new("git")
-                    .arg("describe")
-                    .arg("--tags")
-                    .arg("--dirty")
-                    .arg("--always")
-                    .arg("--long")
-                    .arg("--abbrev=8")
-                    .output()
-                    .ok()
-                    .and_then(|o| String::from_utf8(o.stdout).ok())
-                    .map(|mut s| {
-                        let len = s.trim_end().len();
-                        s.truncate(len);
-                        s
-                    })
-                    .unwrap();
-        let parts = git_describe.split("-").collect::<Vec<_>>();
-        version = format!("{}+{}.{}", parts[0], parts[1], parts[2]);
-    } else {
-        version = "unknown".to_owned();
-    }
-
-    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
-    let mut f = File::create(out_dir.join("git-describe")).unwrap();
-    write!(f, "{}", version).unwrap();
-}
-
 fn linker_script() {
     // Put the linker script somewhere the linker can find it
     let out = &PathBuf::from(env::var_os("OUT_DIR").unwrap());
@@ -61,5 +22,4 @@ fn linker_script() {
 
 fn main() {
     linker_script();
-    git_describe();
 }
